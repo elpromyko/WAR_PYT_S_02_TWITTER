ac92339769cb86e9d3ecfa1cfef48284c5fdd9e4
Piotr Prominski <piotr.prominski@gmail.com>
2017-03-17 20:32:13 +0100

    Add load_tweet_by_id method

--- models/tweet.py
+++ models/tweet.py
@@ -11,5 +11,21 @@ class Tweet(object):
         self.text = ""
         self.creation_date = None
 
-    def load_tweet_by_id(self):
+    @staticmethod
+    def load_tweet_by_id(cursor, id):
+        sql_query = """
+        SELECT user_id, text, creation_date FROM Tweets WHERE id={}
+        """.format(id)
+        cursor.execute(sql_query)
+        row = cursor.fetchone()         #tworzy obiekt ktory jest krotka wartosci z kolumn w rzedzie o danym id
+
+        if row is not None:
+            loaded_tweet = Tweet()
+            loaded_tweet.id = id
+            loaded_tweet.user_id = row[0]
+            loaded_tweet.text = row[1]
+            loaded_tweet.creation_date = row[2]
+            return loaded_tweet
+        else:
+            return None
 
